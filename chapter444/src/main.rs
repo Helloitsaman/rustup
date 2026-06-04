9757c03a579471b2c0f8a2c39c48f43c5c1b29dc
sawantfan16 <amandsouza41@gmail.com>
2026-06-05 01:37:40 +0530

    Initial commit

--- chapter444/src/main.rs
+++ chapter444/src/main.rs
@@ -1,8 +1,31 @@
 fn main() {
     let s=String::from("Hello, world!");
     let mut m=String::from("Hello bird!");
-    
     m.push_str(" No birds");
     println!("{s}");
     println!("{m}");
+    let k=7;
+    jthu(k);
+    println!("{k}");
+    let m=String::from("Ahoy");
+    println!("{m}");
+
+    let g=s.clone();
+    println!("{g}");
+
+    let str=String::from("Hello");
+    bs(str);
+    println!("{str}");
+
 }
+
+fn jthu(x:i32){
+    let k=String::from("Hello, world!");
+    println!("{k} but number is {x}");
+}
+
+fn bs(y:String){
+    println!("{y} World");
+}
+
+
