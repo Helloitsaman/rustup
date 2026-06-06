fc6fd9215df106937af96510906fee0c2a2eb4f3
sawantfan16 <amandsouza41@gmail.com>
2026-06-07 02:10:06 +0530

    Initial commit

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut a=String::from("Aman Souza");
+    let mut b=String::from("Amansouza@gmail.com");
+    user4(&mut a, &mut b);
+    let users=user2(
+        String::from("Amah"),
+        String::from("emahl")
+    );
+
+    let user1 = User {
+        name:String::from("Aman ddd"),
+        email:String::from("emailtypeshi"),
+        number:12345,
+    };
+
+    let useru=User{
+        number:23456,
+        ..user1
+    };
+
+}
+
+struct User {
+    name :String,
+    email:String,
+    number:u64,
+}
+
+
+fn user4(name1:&mut String, email1:&mut String)->User{
+    User{
+        name:name1.to_string(),
+        email:email1.to_string(),
+        number:23456,
+    }
+}
+
+fn user2(name:String, email:String)->User{
+    User{
+        name,
+        email,
+        number:12345
+    }
 }
