701585d7a70a36efea6e56aa731977a5b4106863
sawantfan16 <amandsouza41@gmail.com>
2026-06-20 23:02:41 +0530

    initial commit

--- testing5/src/main.rs
+++ testing5/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::cmp::Ordering;
 fn main() {
-    println!("This code consists of guessing game hardcoded as well as a calci");
+    println!("This code consists of guessing game hardcoded as well as my own rustup calci");
     println!("This was made without ai on my momma and everything i love cuh");
     let r1=Rectangle{
         length:20,
